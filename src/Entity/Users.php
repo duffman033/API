a277aa2c41b81667a684f1d770d6ac72628f0a60
Bastien Moreau <bmoreau72@gmail.com>
2022-11-08 10:45:11 +0100

    Ajout entity Users et validation des données

---
+++ src/Entity/Users.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\UsersRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: UsersRepository::class)]
+#[ApiResource]
+class Users
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 25)]
+    #[Assert\NotBlank (message: "Le nom d'utilisateur est obligatoire")]
+    #[Assert\Length (min: 1, max: 25, minMessage: "Le nom doit faire au moins {{limit}} caractères", maxMessage: "Le nom doit faire au maximum {{limit}} caractères")]
+    private ?string $username = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank (message: "L'email est obligatoire")]
+    #[Assert\Length (min: 1, max: 255, minMessage: "L'email doit faire au moins {{limit}} caractères", maxMessage: "L'email doit faire au maximum {{limit}} caractères")]
+    #[Assert\Email( message: "L'e-mail {{ value }} n'est pas un e-mail valide.")]
+    private ?string $email = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
